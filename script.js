d8daf5de744d5ad40c6c1aed4ad0c11e40f064d2
Charles <charles.lamarque69@gmail.com>
2022-12-13 12:31:46 +0100

    Add calculator visuals

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const Operators = {
+    Add: "add",
+    Substract: "substract",
+    Multiply: "multiply",
+    Divide: "divide"
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +19,24 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case Operators.Add:
+            return add(a, b);
+        break;
+
+        case Operators.Substract:
+            return substract(a, b);
+        break;
+
+        case Operators.Multiply:
+            return multiply(a, b);
+        break;
+
+        case Operators.Divide:
+            return divide(a, b);
+        break;
+    }
 }
